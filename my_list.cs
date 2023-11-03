6ec23d58835a480f04081180c08fb0c4a9a631bf
Lars Thielemann <lars.thielemann@assecosol.com>
2023-11-03 08:47:15 +0100

    Fehlerkorrektur

--- my_list.cs
+++ my_list.cs
@@ -15,7 +15,7 @@ class MyList {
       a[i] = i;
     }
 
-    for(int i = 0; i < a.Length) {
+    for(int i = 0; i < a.Length; i++) {
       Console.WriteLine(a[i]);
     }
     
@@ -26,6 +26,7 @@ class MyList {
   
   static void myList() {
     Console.WriteLine("myList");
+    
   }
 
 }
