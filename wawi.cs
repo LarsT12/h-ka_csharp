37e79955cccb0f2349a6c793c809af7cd10a1044
Lars Thielemann <lars.thielemann@assecosol.com>
2024-05-17 09:34:21 +0200

    Vorlesung 6

--- wawi.cs
+++ wawi.cs
@@ -7,34 +7,44 @@ class WarenWirtschaft {
 
     Stock s1 = new Stock("Hauptlager");
  
-    Product p1 = new Product();
-    p1.Number = "987678";
-    p1.Name = "Bürostuhl";
-    p1.Category = "Büromöbel";
-    p1.Price = 70.5;
-    p1.MinOrderQty = 10;
+    Product p1 = new Product("987678", "Bürostuhl", "Büromöbel", 70.5, 10);
+    Product p2 = new Product("983424", "Arbeitstisch", "Büromöbel", 230.0, 5);
+    Product p3 = new Product("981424", "Barhocker");
 
-    Product p2 = new Product();
-    p2.Number = "983424";
-    p2.Name = "Arbeitstisch";
-    p2.Category = "Büromöbel";
-    p2.Price = 230.0;
-    p2.MinOrderQty = 5;
+    p1.PriceProtocol.Add(p1.Price);
+    p1.Price = 89.0;
+
+    p1.PriceProtocol.Add(p1.Price);
+    p1.Price = 91.5;
 
     s1.Products.Add(p1);
     s1.Products.Add(p2);
   
     Console.WriteLine(s1.Products[0].Name + ": " + s1.Products[0].Price + " EUR");
-    Console.WriteLine(p2.Name + ": " + p2.Price + " EUR");
+    Console.WriteLine(p2.Number + " (" + p2.Name + "): " + p2.Price + " EUR");
   }
 }
 
 class Product {
-  public string Number;
+  public string Number { get; private set; }
   public string Name;
   public double Price;
+  public List<double> PriceProtocol = new List<double>();
   public string Category;
   public int MinOrderQty;
+
+  public Product(string number, string name) {
+    Number = number;
+    Name = name;
+  }
+
+  public Product(string number, string name, string category, double price, int minOrderQty) {
+    Number = number;
+    Name = name;
+    Category = category;
+    Price = price;
+    MinOrderQty = minOrderQty;
+  }
 }
 
 class Stock {
