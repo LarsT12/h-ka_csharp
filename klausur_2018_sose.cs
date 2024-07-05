3642a588e62f2bcff9267d8fcc0368f101055e20
Lars Thielemann <lars.thielemann@assecosol.com>
2024-07-05 15:25:49 +0200

    Vorlesung 12

---
+++ klausur_2018_sose.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+class Klausur2018SoSe {
+  public static void Main() {
+    Console.WriteLine("Klausur 2018 SoSe");
+    aufg3a();
+    aufg3b();
+  }
+
+  public static void aufg3a() {
+
+  }
+
+  public static void aufg3b() {
+    
+  }
+}
