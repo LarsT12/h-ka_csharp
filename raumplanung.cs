dfe1be6cd5afbeb774b3d05097502ed72b0c3a23
Lars Thielemann <lars.thielemann@assecosol.com>
2024-11-21 09:33:02 +0100

    Vorlesung 5

--- raumplanung.cs
+++ raumplanung.cs
@@ -1,1 +1,51 @@
-raumplanung
+using System;
+
+class Raumplanung {
+  static void Main() {
+    Console.WriteLine("Raumplanung H-KA gestaret...");
+    Raum r1 = new Raum('E', "010");
+    r1.Kapazitaet = 21;
+
+    Raum r2 = new Raum('E', "011", 17);
+
+    Console.WriteLine(r1.Gebaeude + r1.Nummer + ": " + r1.Kapazitaet);
+    Console.WriteLine(r2.Gebaeude + r2.Nummer + ": " + r2.Kapazitaet);
+  }
+}
+
+class Kalender {
+
+}
+
+class Vorlesung {
+  public string Name;
+
+}
+
+class Raum {
+  public char Gebaeude { get; }
+  public string Nummer { get; }
+  public int Kapazitaet;
+
+  public Raum(char Gebaeude, string Nr) {
+    this.Gebaeude = Gebaeude;
+    Nummer = Nr;
+    Console.WriteLine("Raum " + this.Gebaeude + Nummer + " erzeugt");
+  }
+
+  public Raum(char Gebaeude, string Nr, int kapa) {
+    this.Gebaeude = Gebaeude;
+    Nummer = Nr;
+    Kapazitaet = kapa;
+    Console.WriteLine("Raum " + this.Gebaeude + Nummer + " erzeugt");
+  }
+}
+
+class Studiengruppe {
+  public string Name;
+  public int Groesse;
+}
+
+class Dozent {
+  public string Name;
+}
