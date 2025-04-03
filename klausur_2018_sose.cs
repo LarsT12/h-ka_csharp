6bcbb08dbea30f1ef5a61ff34eb9d4057e5e6b08
Lars Thielemann <lars.thielemann@assecosol.com>
2025-04-03 21:28:06 +0200

    Klausur 2018 SoSe und WiSe

---
+++ klausur_2018_sose.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+class Klausur2018SoSe {
+  public static void Main() {
+    Console.WriteLine("Klausur 2018 WiSe");
+    // aufg2a();
+    //aufg2b();
+    //aufg2c();
+    //aufg2d();
+    //aufg2e();
+  }
+}
