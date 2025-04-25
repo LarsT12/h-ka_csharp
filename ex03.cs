e91f5c253552810c2228cc5328a1b3cc1f3a2eb7
Lars Thielemann <lars.thielemann@assecosol.com>
2025-04-25 09:34:21 +0200

    Vorlesung 3

--- ex03.cs
+++ ex03.cs
@@ -3,7 +3,30 @@ using System;
 class Wochenablauf {
 
   static void Main(string[] args) {
-    Console.WriteLine("Wochenablauf");
+    string[] wochentag = new string[] { "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So" };
+
+    Console.WriteLine("Wochenablauf\n");
+
+    for(int i = 0; i < wochentag.Length; ++i) {
+      Console.WriteLine("### " + wochentag[i] + " ###");
+
+      if(i < 5) { // boolscher Ausdruck, der Abhängigkeit einer veränderbaren Variable steht
+        Console.WriteLine("Wecker klingelt");
+        Console.WriteLine("Aufstehen");
+      } else { // ansonsten ...
+        Console.WriteLine("Sonne geht auf");
+        Console.WriteLine("Erst mal liegen bleiben");
+      }
+      
+      Console.WriteLine("Toilette");
+      Console.WriteLine("Duschen");
+      Console.WriteLine("Erwartete Temp.: " + args[i] + "°C");
+      Console.WriteLine("Frühstück");
+      Console.WriteLine("Los");
+      Console.WriteLine("");
+    }
+
+    Console.WriteLine("Wochenende");   
 
   }
 }
--- exercise03.md
+++ exercise03.md
@@ -2,9 +2,13 @@
 
 ## Aufg. 1
   Erstelle eine Klasse "Wochenablauf", welche die Morgenroutine aus Übung 1 als Wiederholung für jeden Wochentag enthält.
-  Führe eine neue Variable wochentag als String-Array ein und weise ihr die Wochentage "Mo", "Di" ... zu.
-  Gib in der Schleife, die deine Morgenroutine ausgibt, nun als erstes den Wochentag aus.
+  Führe eine neue Variable wochentag als String-Array ein und weise ihr die Namen der Wochentage "Mo", "Di" ... zu.
+  Gib in der Schleife, die deine Morgenroutine ausgibt, nun als erstes den den Namen des Wochentags aus.
 
 ## Aufg. 2
   Verwende für die max. Anzahl an Wiederholungen die Größe deines Arrays.
+
+## Aufg. 3
+  Verwende int[] args als Parameter in der Main-Methode. Gib dazu in deiner Morgenroutine in einer weiteren Ausgabe die erwartete Temp. für den Tag an.
+  Dazu werden beim Aufrufen des Programms sieben Temp.-Werte angegeben.
 
