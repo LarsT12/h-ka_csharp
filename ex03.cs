055690d025f70bb91cc022b4eebc7ab572720dd2
Lars Thielemann <mail@lars-thielemann.de>
2025-12-05 08:10:54 +0100

    V7

--- ex03.cs
+++ ex03.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 class Strukturen {
   static void Main(string[] args) {
+    int maxL = Kontakt.MaxVornameLaenge;
+
     List<Kontakt> Adressbuch = new List<Kontakt>();
     Adressbuch.Add(new Kontakt("Lars", "Larsson", "lars@lars.son"));
     Kontakt a = new Kontakt("Anna", "Andersdotir", "andra@gmx.com");
@@ -16,34 +18,64 @@ class Strukturen {
     }
     Kontakt b = new Kontakt(vorname, "Petersen", "p@p.com", "0989 24545", "Beispielweg 5, 54321 Beispielstadt");
     Adressbuch.Add(b);
-    Console.WriteLine(b.ToString());
+    // Console.WriteLine(b.ToString());
+    b.Call();
 
     foreach(Kontakt k in Adressbuch) {
-      Console.WriteLine(k);
+      Console.WriteLine(k.Vorname);
     }
+
+    b.SetNachname("NeuerNachname");
+    b.Nachname = "NochEinNeuerNachnameNochEinNeuerNachnameNochEinNeuerNachnameNochEinNeuerNachnameNochEinNeuerNachname";
+    Console.WriteLine(b.Nachname);
+    // Console.WriteLine($"Vorname: {Kontakt.FindByEmail(Adressbuch, "p@p.com").Vorname}");
   }
 }
 
 class Kontakt {
    // Klassenvariablen
   public static int anzahlKontakte = 0;
-  public static int MaxVornameLaenge = 10;
+  public const int MaxVornameLaenge = 10;
+  public const int MaxNachnameLaenge = 30;
   
   // Objektvariablen
-  public string Vorname;
-  public string Nachname;
+  public string Vorname { get; private set; }
+
+  private string _nachname;
+  public string Nachname {
+    get {
+      return _nachname;
+    }
+    set {
+      if(value.Length > MaxNachnameLaenge) {
+        throw new ArgumentException($"Nachname darf maximal {MaxNachnameLaenge} Zeichen lang sein.");
+        //_nachname = value.Substring(0, MaxNachnameLaenge - 3) + "...";
+      } else {
+        _nachname = value;
+      }
+    }
+  }
+  public void SetNachname(string v) {
+    _nachname = v;
+  }
+
   public string Emailadresse;
   public string Telefonnummer;
   public string Adresse;
+
+  public static Kontakt FindByEmail(List<Kontakt> adressbuch, string email) {
+    foreach(Kontakt k in adressbuch) {
+      if(k.Emailadresse == email) {
+        return k;
+      }
+    }
+    return null;
+  }
 
   public Kontakt(string Vorname, string n, string m) {
     this.Vorname = Vorname;
     Nachname = n;
     Emailadresse = m;
-  }
-
-  private int foo() {
-    return MaxVornameLaenge;
   }
   
   public Kontakt(string v, string n, string m, string t, string a) {
@@ -57,6 +89,8 @@ class Kontakt {
   public static string NormalisiereTelefonnummer(string t) {
     return t.Replace(" ", "").Replace("-", "");
   }
+
+
 
   public void Call() {
     Console.WriteLine($"Rufe {Vorname} {Nachname} unter {Telefonnummer} an...");
