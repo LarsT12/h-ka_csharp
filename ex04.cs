707c72fdbdc8387ce4d700b6bc17a83575fd3035
Lars Thielemann <mail@lars-thielemann.de>
2025-12-12 23:40:22 +0100

    V9

--- ex04.cs
+++ ex04.cs
@@ -3,18 +3,44 @@ using System.Collections.Generic;
 
 class Test {
   static void Main(string[] args) {
-    A foo = new A();
-    Console.WriteLine(foo);
+    Console.WriteLine(A.getDescription());
+    
+    A a = new A(3);
+    Console.WriteLine(a.calculateSomething());
+    Console.WriteLine(a);
+
+    B b = new B();
+    Console.WriteLine(b);
+  
+  }
+  
+  class B {
+    public override string ToString() {
+      return "Ich bin die Klasse B.";
+    }
   }
 }
 
 class A {
+  public static string getDescription() {
+    return "Ich bin die Klasse A.";
+  }
+
+  private int someValue;
+  public int calculateSomething() {
+    return someValue * 7;
+  }
+
+  public A(int someValue) {
+    this.someValue = someValue;
+  }
 
   public override string ToString() {
-    return $"Ich bin ein A-Objekt mit dem Hashcode {GetHashCode()}.";
+    return $"Ich bin ein A-Objekt mit der Wertigkeit {someValue}.";
   }
 
   public override int GetHashCode() {
     return 42;
   }
+
 }
